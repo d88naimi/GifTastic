8ce27e4e258fd23b3f82ae3513e56605f23083a1
David Naimi <d88naimi@gmail.com>
2017-02-24 14:54:47 -0800

    yayaya

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,86 +1,102 @@
- console.log("Goodday");
-
+ 
 
  var zooArray = [ "squirrel" , "dog", "cat", "lion" , " tiger", "rat", "rhino" ];
-renderButtons();
-
- // This .on("click") function will trigger the AJAX Call
- // function display animal 
-
+   
 function displayAnimal (){
 
 // animalInfo function re-renders the HTML to display the appropriate content
 var animal = $(this).attr("data-name");
-    animal = encodeURI(animal);
+  
 
 var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
         animal + "&api_key=dc6zaTOxFJmzC&limit=10";
-
+         
 // Creating an AJAX call for the specific movie button being clicked
         $.ajax({
           url: queryURL,
           method: "GET"
         }).done(function(response) {
+
+          for (let i = 0; i < response.data.length; i++){
+          // create a div to store animalrespnse
+          var animalDiv = $('<div class="animalDiv">');
+          // stored rating 
+          var rating = response.data[i].rating; 
+          // create a paragraph to store rating value
+          var p = $("<p>").text("Rating : " + rating);
+          // response for still & response for animated 
+          var animate = response.data[i].images.fixed_height.url;
+
+          var still = response.data[i].images.fixed_height_still.url;
           
-          console.log(response);
-           // Creating a div to hold the animal
-           var animalDiv = $("<div class = 'animal'>");
-           // Storing the rating data
-           var rating = response.Rated;
-           // Creating an element to have the rating displayed
-           var pOne = $("<p>").text("Rating: " + rating);
-           // Retrieving the URL for the image
-           var imgURl = response.Poster;
-           // Creating an element to hold the image
-           var image = $("<img>").attr("src", imgURl);
-           // Appending the image
-           animalDiv.append(image);
-           $("data-name").prepend(animalDiv);
+          var img = $('<img class="imageGyphy">');
+
+           img.attr("src", still );
+           img.attr("data-still", still);
+           img.attr("data-animate", animate);
+           img.attr("data-state" , "still");
+           // image  is being append to animal Div 
+           animalDiv.append(img);
+           // animalDiv is appeniding to the paragrapgh div 
+           p.appendTo(animalDiv);
+
+           $("#gif-holder").prepend(animalDiv);
+         }
+
         });
       }
 
-      // Function for displaying animal data
-      function renderButtons() {
-         // Deleting the movies prior to adding new movies
-        // (this is necessary otherwise you will have repeat buttons)
-        $("#buttons-view").empty();
+        $("#gif-holder").on("click" ,".imageGyphy", function(){
+
+          var state = $(this).attr("data-state");
+          
+          if (state == "still"){
+            $(this).attr("src" , $(this).data("animate"));
+             $(this).attr("data-state" , "animate");}
+
+          else { 
+              $(this).attr("src" , $(this).data("still"));
+             $(this).attr("data-state" , "still");
+          }
+
+        });
+    
+  
+// function display animal 
+   function renderButtons() {
+        
+        $("#newButtons").empty();
         // Looping through the array of zoo
         for (var i = 0; i < zooArray.length; i++) {
           // Then dynamicaly generating buttons for each movie in the array
           // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
           var a = $("<button>");
-          // Adding a class of movie to our button
+          // Adding a class of  to our button
           a.addClass("zoo")
            // Adding a data-attribute
-          a.attr("data-name" ,  zooArray[i]);
+          a.attr("data-name" , zooArray[i]);
           // Providing the initial button text
           a.text(zooArray[i]);
-          $("#buttons-view").append(a);
+          
+          $("#newButtons").append(a);
+      }    
 
-      }
-    }
+    };
 
-     // This function handles events where a animal search button is clicked
-     $("#add-animal").on("click" , function(event) {
-      event.preventDefault();
+      $(".add-gify").on("click" , function(event){
+          event.preventDefault();
+ // event.preventDefault();
       // This line grabs the input from the textbox
-      var zoo = $("#find-input").val().trim();
+      var zoo = $(".inputText").val().trim();
       // Adding movie from the textbox to our array
       zooArray.push(zoo);
        // Calling renderButtons which handles the processing of our movie array
        renderButtons();
-     }); // Adding a click event listener to all elements with a class of "zoo"
+     }); 
 
-     $("#find-animal").on("click" , ".zoo" , displayAnimal);
-     // Calling the renderButtons function to display the intial buttons
-     renderButtons();
+      $(document).on("click" , ".zoo" , displayAnimal);
 
-
-
- 
-
-
-  
+      renderButtons();
 
   
 
